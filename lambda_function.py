f13d8112c3a5dd2773e04617025c7d1910f3410d
Iván Otero <ivanoterouvigo@gmail.com>
2024-01-25 10:43:34 +0100

    Update lambda_function.py
    Added MesIntentHandler

--- lambda_function.py
+++ lambda_function.py
@@ -41,3 +41,18 @@ sb.add_request_handler(LaunchRequestHandler())
 
 # The default name in the Lambda console is lambda_function.lambda_handler
 lambda_handler = sb.lambda_handler()
+
+class MesIntentHandler(AbstractRequestHandler):
+    """Handler for Mes Intent"""
+    def can_handle(self, handler_input):
+        state = handler_input.attributes_manager.session_attributes['test_state']
+        full_state = str(state['item']) + str(state['step'])
+        return (ask_utils.is_intent_name("MesIntent")(handler_input) and full_state in ['63'])
+    
+    def handle(self, handler_input):
+        
+        logger.info("In MesIntentHandler")
+        state = handler_input.attributes_manager.session_attributes['test_state']
+        state['month'] = handler_input.request_envelope.request.intent.slots['mes'].value
+        return NextIntentHandler().handle(handler_input)
+
